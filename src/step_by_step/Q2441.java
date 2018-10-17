ceedc73600ce109cf56f4f723a0090eb5b7fb2b7
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-10-17 23:06:58 +0900

    1017
    .

--- src/step_by_step/Q2441.java
+++ src/step_by_step/Q2441.java
@@ -2,18 +2,18 @@ package step_by_step;
 
 import java.util.Scanner;
 
-public class Q2439 {
-
+public class Q2441 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int count =0;
 		for(int i=n; i>0; i--) {
-			for(int j=i-1; j>0; j--) 
-				System.out.print(" ");
-			count ++;
 			for(int z=0; z<count; z++)
+				System.out.print(" ");
+			for(int j=i; j>0; j--) 
 				System.out.print("*");
+			count ++;
+			
 			if(i!= 1)
 			System.out.println();
 		}
