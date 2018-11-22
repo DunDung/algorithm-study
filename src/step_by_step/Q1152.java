cec0db95f2b4a0520a226dc946ade044894cf545
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-11-22 17:44:24 +0900

    1122
    2문제

---
+++ src/step_by_step/Q1152.java
@@ -0,0 +1,17 @@
+package step_by_step;
+
+import java.util.Scanner;
+
+public class Q1152 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		if(s.trim().length() == 0)
+			System.out.println("0");
+		else {
+			String [] a = s.trim().split(" ");
+			System.out.println(a.length);
+		}
+	}
+}
