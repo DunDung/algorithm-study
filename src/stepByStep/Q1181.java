95976c6ec96594c598de88b2c35fd5856c00373a
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2019-01-01 17:01:00 +0900

    0101
    단어 정렬

---
+++ src/stepByStep/Q1181.java
@@ -0,0 +1,43 @@
+package stepByStep;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Q1181 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		Set<String> set = new HashSet<>();
+		for(int i=0; i<n; i++) {
+			set.add(scan.next());
+		}
+		Data [] a = new Data[set.size()];
+		Iterator<String> it = set.iterator();
+		for(int i=0; i<a.length; i++) {
+			a[i] = new Data(it.next()); 
+		}	
+		Arrays.sort(a);
+		for(Data d: a)
+			System.out.println(d.s);
+	}
+	
+}
+class Data implements Comparable<Data>{
+	String s;
+
+	public Data(String s){
+		this.s = s;
+	}
+	
+	@Override
+	public int compareTo(Data o) {
+		int r = this.s.length()-o.s.length();
+		if(r==0)
+			r = this.s.compareTo(o.s);
+		return r;		
+	}
+}
