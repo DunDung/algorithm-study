026fe063082e7f8ef403aa9b4ab3301458affc52
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2019-01-11 16:46:39 +0900

    20190111
    .

---
+++ src/stepByStep/Q1978.java
@@ -0,0 +1,27 @@
+package stepByStep;
+
+import java.util.Scanner;
+
+public class Q1978 {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		int count = 0;
+		int n =scan.nextInt();
+		for(int i=0; i<n; i++) {
+			int x = scan.nextInt();
+			boolean b = false;
+			if(x==1)
+				continue;
+			for(int j=2; j<x; j++) {
+				if(x%j==0) {
+					b = true;
+					break;
+				}
+			}
+			if(!b)
+				count++;
+		}
+		System.out.println(count);
+	}
+}
