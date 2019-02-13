d65d816a0c56f00ddca50a39d0f5131ff26e29f1
DunDung <ebseud6135@naver.com>
2019-02-13 14:06:39 +0900

    20190213
    .

--- src/baekjoon/Q2501.java
+++ src/baekjoon/Q2501.java
@@ -1,13 +1,24 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Q2501 {
 
 	public static void main(String[] args) {
-		System.out.println("       _.-;;-._");
-		System.out.println("'-..-'|   ||   |");
-		System.out.println("'-..-'|_.-;;-._|");
-		System.out.println("'-..-'|   ||   |");
-		System.out.println("'-..-'|_.-''-._|");
+		Scanner scan = new Scanner(System.in);
+		List<Integer> list = new ArrayList<>();
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		for(int i=1; i<=n; i++)
+			if(n%i==0)
+				list.add(i);
+		
+		if(list.size()<=k-1)
+			System.out.println(0);
+		else
+			System.out.println(list.get(k-1));
 	}
 
 }
