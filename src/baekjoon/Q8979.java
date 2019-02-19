455be650d45ef0efef7b496e81c89243cebc4492
DunDung <ebseud6135@naver.com>
2019-02-19 17:22:20 +0900

    20190219
    .

---
+++ src/baekjoon/Q8979.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q8979 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t=scan.nextInt();
+		int goal=scan.nextInt();
+		int [][] a = new int[t][4];
+		for(int i=0; i<t; i++)
+			for(int j=0; j<4; j++)
+				a[i][j] = 
+	
+	}
+}
+
