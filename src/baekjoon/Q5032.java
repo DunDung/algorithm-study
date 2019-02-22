f0bc45e1a4c69f51e6b8103b303fb38cffd0b2f2
DunDung <ebseud6135@naver.com>
2019-02-23 02:49:02 +0900

    20190223

---
+++ src/baekjoon/Q5032.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q5032 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int e = scan.nextInt();
+		int f = scan.nextInt();
+		int c = scan.nextInt();
+		
+		
+	}
+
+}
