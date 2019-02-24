eaaaa063295c2568d08f7d3321ba8e5f61b43b88
DunDung <ebseud6135@naver.com>
2019-02-24 23:56:05 +0900

    20190224

--- src/baekjoon/Q5032.java
+++ src/baekjoon/Q5032.java
@@ -10,7 +10,16 @@ public class Q5032 {
 		int f = scan.nextInt();
 		int c = scan.nextInt();
 		
-		
+		int count = (e+f)/c;
+		count += a((e+f)%c+count, c);
+		System.out.println(count);
+	}
+	
+	static int a(int left, int c) {
+		if((left%c)+(left/c) >= 3)
+			return left/c+a(left%c+left/c, c);
+		else
+			return left/c;
 	}
 
 }
