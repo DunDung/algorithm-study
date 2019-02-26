cf8d04bab5f8e92d87158b43659a42855a45be7f
DunDung <ebseud6135@naver.com>
2019-02-26 17:20:18 +0900

    Update Q8979.java

--- src/baekjoon/Q8979.java
+++ src/baekjoon/Q8979.java
@@ -9,11 +9,30 @@ public class Q8979 {
 		Scanner scan = new Scanner(System.in);
 		int t=scan.nextInt();
 		int goal=scan.nextInt();
-		int [][] a = new int[t][4];
-		for(int i=0; i<t; i++)
-			for(int j=0; j<4; j++)
-				a[i][j] = 
-	
+		int [][] a = new int[t][3];
+		int count = 1;
+		
+		for(int i=0; i<t; i++) {
+			int k = scan.nextInt();
+			for(int j=0; j<3; j++) { 
+				a[k-1][j] = scan.nextInt();
+			}
+		}				
+
+
+		for(int i=0; i<t; i++) { 
+			if(a[goal-1][0]<a[i][0]) {
+				count++;
+			}
+			else if(a[goal-1][0]==a[i][0]) {
+				if(a[goal-1][1]<a[i][1])
+					count++;
+				else if(a[goal-1][1]==a[i][1])
+					if(a[goal-1][2]<a[i][2])
+						count++;
+			}
+		}
+		System.out.println(count);
 	}
 }
 
