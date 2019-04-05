b9e274f1572d3897af69767444ddbe991dbe232e
DunDung <ebseud6135@naver.com>
2019-04-05 18:01:49 +0900

    20190405

--- src/baekjoon/Q1676.java
+++ src/baekjoon/Q1676.java
@@ -3,25 +3,22 @@ package baekjoon;
 import java.util.Scanner;
 
 //팩토리얼 0의 개수
+//X
+//2를 인자로 가진 개수와 5를 인자로 가진 개수중 작은것이 0의 자리수를 나타낸다.
+
 public class Q1676 {
-	static long factorial(int n) {
-		if(n<=1)
-			return 1;
-		return n*factorial(n-1);
-	}
+	static int two = 0;
+	static int five = 0;
 	public static void main(String [] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int count = 0;
-		System.out.println(factorial(n));
-		String s = String.valueOf(factorial(n));
-		for(int i=s.length()-1; i>=0; i--) {
-			if(s.substring(i, i+1).equals("0"))
-				count++;
-			else
-				break;
-		}
-		System.out.println(count);
+	    for (int i = 2; i <= n; i *= 2) 
+	        two += n / i;
+	    
+	    for (int i = 5; i <= n; i *= 5) 
+	        five += n / i;
+	 
+	    System.out.printf("%d\n", (two < five) ? two : five);
 		scan.close();
 	}
 }
