f3624065be42c447ad96816d06ed3a92bcbfa90f
DunDung <ebseud6135@naver.com>
2019-04-10 15:54:53 +0900

    20190410

--- src/baekjoon/dp/Q11057.java
+++ src/baekjoon/dp/Q11057.java
@@ -2,34 +2,31 @@ package baekjoon.dp;
 
 import java.util.Scanner;
 
-//쉬운 계단 수
-//X
-public class Q10844 {
+//오르막 수
+//O!
+public class Q11057 {
 
 	public static void main(String[] args) {
-		final int mod = 1000000000;
+		final int mod = 10007;
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		long [][] dp = new long[n+1][10];
-		
-		for(int i=1; i<10; i++)
+
+		for(int i=0; i<10; i++)
 			dp[1][i] = 1;
-		
+
 		for(int i=2; i<=n; i++) {
 			for(int j=0; j<10; j++) {
-				if(j == 0)
-					dp[i][j] = dp[i-1][j+1];
-				else if(j == 9)
-					dp[i][j] = dp[i-1][j-1];
-				else
-					dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1];
-				dp[i][j] %= mod;
+				for(int k=j; k<10; k++) {
+					dp[i][j] += dp[i-1][k];
+					dp[i][j] %= mod;
+				}
 			}
 		}
 		long answer = 0;
 		for(int i=0; i<10; i++)
 			answer += dp[n][i];
-		
+
 		System.out.println(answer%mod);
 	}
 }
