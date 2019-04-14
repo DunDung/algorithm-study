21f22d9a50185d47cbaab705f5b50f4b9abb8716
DunDung <ebseud6135@naver.com>
2019-04-14 18:39:01 +0900

    20190414

--- src/baekjoon/dp/Q2579.java
+++ src/baekjoon/dp/Q2579.java
@@ -8,20 +8,34 @@ public class Q2579 {
 	public static void main(String [] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int [] array = new int[n];
-		int [] dp = new int[n];
+		int [] a = new int[n+1];
+		int [] dp = new int[n+1];
 		
-		for(int i=0; i<n; i++) 
-			array[i] = scan.nextInt();
+		for(int i=1; i<=n; i++) 
+			a[i] = scan.nextInt();
 		
-		dp[0] = array[0]; //첫번 째 계단
-		dp[1] = Math.max(array[1], dp[0]+array[1]); //두번 째 계단은 첫번 째 계단만 또는 첫번 째 계단 + 두번 째 계단
-		dp[2] = Math.max(array[0]+array[2], array[1]+array[2]); //세번 째 계단은 첫번째 계단 + 세번째 계단 
-															//또는 두번 째 계단 + 세번 째 계단
+		dp[1] = a[1];
+		dp[2] = a[1]+a[2]; 
 		
-		for(int i=3; i<n; i++) 
-			dp[i] = Math.max(dp[i-2]+array[i], dp[i-3]+array[i-1]+array[i]);
-			//전전 계단 + 지금 계단 또는 전전전 계단 +전 계단 + 지금 계단
-		System.out.println(dp[n-1]);
+		for(int i=3; i<=n; i++) 
+			dp[i] = Math.max(dp[i-2]+a[i], dp[i-3]+a[i-1]+a[i]);
+		System.out.println(dp[n]);
+	
+// 2차원으로 풀이
+//		Scanner scan = new Scanner(System.in);
+//		int n = scan.nextInt();
+//		int [] a = new int[n+1];
+//		int [][]dp = new int[n+1][2];
+//
+//		for(int i=1; i<=n; i++)
+//			a[i] = scan.nextInt();
+//		
+//		dp[1][0] = a[1];
+//		
+//		for(int i=2; i<=n; i++) {
+//			dp[i][0] = Math.max(dp[i-2][0],dp[i-2][1])+a[i]; //첫번째 계단인 경우
+//			dp[i][1] = dp[i-1][0]+a[i]; //두번 째 계단인 경우
+//		}
+//		System.out.println(Math.max(dp[n][0],dp[n][1]));
 	}
 }
