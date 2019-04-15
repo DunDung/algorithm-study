d33522520180d4379c903530a5cbb85ffa0dd3a4
DunDung <ebseud6135@naver.com>
2019-04-15 23:33:14 +0900

    20190415

---
+++ src/baekjoon/math/Q11005.java
@@ -0,0 +1,26 @@
+package baekjoon.math;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//진법 변환2
+//X
+public class Q11005 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int b = scan.nextInt();
+		List<Character> list = new ArrayList<>();
+		while(n>0) {
+			if(n%b<10)
+				list.add((char)(n%b+'0'));
+			else
+				list.add((char)(n%b-10+'A'));
+			n/=b;
+		}
+		for(int i=list.size()-1; i>=0; i--)
+			System.out.print(list.get(i));
+	}
+}
