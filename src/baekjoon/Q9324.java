167c7133e9f76c03a81e3fa28e0a321ecef26e02
DunDung <ebseud6135@naver.com>
2019-04-20 03:28:38 +0900

    20190420

--- src/baekjoon/Q9324.java
+++ src/baekjoon/Q9324.java
@@ -18,19 +18,22 @@ public class Q9324 {
 			for(int j=0; j<s.length(); j++) {
 				if(map.containsKey(s.substring(j, j+1))) {
 					map.replace(s.substring(j, j+1), map.get(s.substring(j, j+1)), map.get(s.substring(j, j+1))+1);
+					if(map.get(s.substring(j, j+1))%3 == 0) {
+						if(j == s.length()-1)
+							isOk = false;
+						else if(!s.substring(j, j+1).equals(s.substring(j+1, j+2)))
+							isOk = false;
+					}
+						
 				}
 				else
 					map.put(s.substring(j, j+1), 1);
 			}
-			for(int j=0; j<s.length(); j++) {
-				if(map.get(s.substring(j, j+1))%3 == 0) {
-					System.out.println("FAKE");
-					isOk = false;
-					break;
-				}
-			}
 			if(isOk)
 				System.out.println("OK");
+			else
+				System.out.println("FAKE");
+			
 				
 		}
 	}
