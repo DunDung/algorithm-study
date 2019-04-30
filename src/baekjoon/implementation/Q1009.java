1c4eba964633e3165ff3f1a7917784dc517e3898
DunDung <ebseud6135@naver.com>
2019-05-01 02:53:06 +0900

    20190501

--- src/baekjoon/implementation/Q1009.java
+++ src/baekjoon/implementation/Q1009.java
@@ -1,6 +1,5 @@
 package baekjoon.implementation;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 //분산처리
@@ -16,7 +15,10 @@ public class Q1009 {
 				k*=a;
 				k=k%10;
 			}
-			System.out.println(k%10);
+			if(k%10 == 0) //10번 째 컴퓨타
+				System.out.println(10);
+			else
+				System.out.println(k%10);
 		}
 	}
 }
