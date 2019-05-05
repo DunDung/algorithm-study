0e8413a931ab42b24d02f8daeba12334d6bef4b0
DunDung <ebseud6135@naver.com>
2019-05-06 02:35:15 +0900

    20190506

---
+++ src/baekjoon/math/Q2163.java
@@ -0,0 +1,14 @@
+package baekjoon.math;
+
+import java.util.Scanner;
+
+//초콜릿 자르기
+public class Q2163 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+
+		System.out.println(n*m-1);
+	}
+}
