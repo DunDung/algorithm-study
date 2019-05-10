64d9af325d4fd4e99bc4c2bf12a13dc1b4458e9e
DunDung <ebseud6135@naver.com>
2019-05-11 02:55:15 +0900

    20190511

---
+++ src/baekjoon/math/Q11816.java
@@ -0,0 +1,35 @@
+package baekjoon.math;
+
+import java.util.Scanner;
+
+public class Q11816 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		int b = 0;
+		if(s.charAt(0)-'0' == 0) {
+			if(s.charAt(1)=='x') {
+				b=16;
+				s = s.substring(2, s.length());
+			}
+			else {
+				b=8;
+				s=s.substring(1, s.length());
+			}
+		}
+		else
+			b = 10;
+		
+		int ans = 0;
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)-'0'<10)
+				ans = ans*b+(s.charAt(i)-'0');
+			else
+				ans = ans*b+(s.charAt(i)+10-'a');
+		}
+		System.out.println(ans);
+		
+	}
+
+}
