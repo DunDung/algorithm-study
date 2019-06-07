f2bfb183c77f8c7ce73d7498e62f8a6a29ba254d
DunDung <ebseud6135@naver.com>
2019-06-07 19:37:18 +0900

    20190607

--- src/baekjoon/bruteforce/Q6603.java
+++ src/baekjoon/bruteforce/Q6603.java
@@ -5,51 +5,83 @@ import java.util.List;
 import java.util.Scanner;
 
 //로또	
+//X
 public class Q6603 {
-	public static boolean nextPermutation(int [] a) {
-		
+	public static void swap(List<Integer> a, int i, int j) {
+		int temp = a.get(i);
+		a.set(i, a.get(j));
+		a.set(j, temp);
+	}
+
+	public static boolean nextPermutation(List<Integer> a) {
+		int i = a.size()-1;
+		while(i > 0 && a.get(i) <= a.get(i-1))
+			i--;
+
+		if(i < 1)
+			return false;
+
+		int j = a.size()-1;
+		while(a.get(j) <= a.get(i-1))
+			j--;
+
+		swap (a, i-1, j);
+
+		j = a.size()-1;
+		while(i < j) {
+			swap(a, i , j);
+			i++;
+			j--;
+		}
+		return true;
+
 	}
 	public static void main(String[] args) {
 		Scanner scan  = new Scanner(System.in);
 		while (true) {
-	        int n = scan.nextInt();
-	        if (n == 0) {
-	            break;
-	        }
-	      //  vector<int> a(n);
-	        int []a = new int[n];
-	        for (int i=0; i<n; i++) {
-	        	a[i] = scan.nextInt();
-	        }
-//	        vector<int> d;
-	        List<Integer> d = new ArrayList<>();
-	        for (int i=0; i<n-6; i++) {
-	            d.add(0);
-	        }
-	        for (int i=0; i<6; i++) {
-	        	d.add(1);
-	        }
-	       // vector<vector<int>> ans;
-	        List<List<Integer>> ans = new ArrayList<>();
-	        do {
-	        	List<Integer> current = new ArrayList<>();
-//	            vector<int> current;
-	            for (int i=0; i<n; i++) {
-	                if (d.get(i) == 1) {
-	                    current.add(a[i]);
-	                }
-	            }
-	           // ans.push_back(current);
-	            ans.add(current);
-	        } while (next_permutation(d.begin(), d.end()));
-	        sort(ans.begin(), ans.end());
-	        for (auto &v : ans) {
-	            for (int i=0; i<v.size(); i++) {
-	                cout << v[i] << ' ';
-	            }
-	            cout << '\n';
-	        }
-	        cout << '\n';
-	}
+			int n = scan.nextInt();
+			if (n == 0) {
+				break;
+			}
+			int []a = new int[n];
+			for (int i=0; i<n; i++) {
+				a[i] = scan.nextInt();
+			}
+			List<Integer> d = new ArrayList<>();
+			for (int i=0; i<n-6; i++) {
+				d.add(0);
+			}
+			for (int i=0; i<6; i++) {
+				d.add(1);
+			}
+			List<List<Integer>> ans = new ArrayList<>();
+			do {
+				List<Integer> current = new ArrayList<>();
+				for (int i=0; i<n; i++) {
+					if (d.get(i) == 1) {
+						current.add(a[i]);
+					}
+				}
+				ans.add(current);
+			} while (nextPermutation(d));
+			ans.sort((x, y) -> { //List<List<Integer> 를 람다식으로 compareTo를 재정의 후 정렬
+				int r = 0;
+				for(int i=0; i<ans.size(); i++) {
+					r = x.get(i)- y.get(i);
+					if(r!=0)
+						break;
+				}
+				return r;
+			});
+				
+			for(List<Integer> k : ans) { //출력
+				for (int i=0; i<k.size(); i++) {
+					System.out.print(k.get(i)+" ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
 
+	}
 }
