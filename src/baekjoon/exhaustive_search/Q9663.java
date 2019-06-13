786f2a78fec5cc6058f70f0dc1e334e22ce4b8c9
DunDung <ebseud6135@naver.com>
2019-06-13 18:01:15 +0900

    20190613

--- src/baekjoon/exhaustive_search/Q9663.java
+++ src/baekjoon/exhaustive_search/Q9663.java
@@ -5,57 +5,46 @@ import java.util.Scanner;
 //N-Queen
 //X
 public class Q9663 {
-	static int ans, n;
-	static int[] col;
-
-	public static boolean isPossible(int c) {
-		// col[1]의 의미는 1행 *열이다.
-		// col[1] = 1 => 1행 1열, col[2] = 3 => 2행 3열
-		
-		// 이전 열들을 탐색하면서 배치 가능 여부 확인
-		for (int i = 1; i < c; i++) {
-			// 같은 행, 열
-			if (col[i] == col[c]) {
-				return false;
-			}
-			// 대각선
-			if (Math.abs(col[i] - col[c]) == Math.abs(i - c)) {
-				return false;
-			}
+	static boolean [] checkCol = new boolean[15];
+	static boolean [] checkRightDig = new boolean[40];
+	static boolean [] checkLeftDig = new boolean[40];
+	static int n;
+	
+	public static boolean check(int row, int col) {
+		if(checkCol[col]) { 
+			return false;
+		}
+		if(checkRightDig[row+col]) {
+			return false;
+		}
+		if(checkLeftDig[row-col+n]) {
+			return false;
 		}
 		return true;
 	}
-	public static void dfs(int row) {
-	    if (row == n) {
-	        ++ans;
-	    } else {
-	        for (int i = 1; i <= n; i++) {
-	            col[row + 1] = i;
-	            if (isPossible(row + 1)) {
-	                dfs(row + 1);
-	            } else {
-	                col[row + 1] = 0;    
-	            }
-	        }
-	    }
-	    col[row] = 0;
+	
+	public static int calc(int row) {
+		if(row == n) {
+			return 1;
+		}
+		
+		int cnt = 0;
+		for(int col=0; col<n; col++) {
+			if(check(row, col)) {
+				checkCol[col] = true;
+				checkRightDig[row+col] = true;
+				checkLeftDig[row-col+n] = true;
+				cnt += calc(row+1);
+				checkCol[col] = false;
+				checkLeftDig[row+col] = false;
+				checkRightDig[row-col+n] = false;
+			}
+		}
+		return cnt;
 	}
-
-
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		 
-	    for (int i = 1; i <= n; i++) {
-	        col = new int[15];
-	        col[1] = i;
-	        // 정점은 행을 기준. (i = 1) => 1행(1열), (i = 2) => 2행(1열), (i = 3) => 3행(1열) 
-	        dfs(1);
-	    }
-	    System.out.println(ans);
-
-
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt(); 
+		System.out.println(calc(0));
 	}
-
 }
