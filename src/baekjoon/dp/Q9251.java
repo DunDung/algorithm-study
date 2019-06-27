f5fa3d38112a9ace30518fd3ceb4dbc82252654e
DunDung <ebseud6135@naver.com>
2019-06-27 23:44:48 +0900

    20190627

--- src/baekjoon/dp/Q9251.java
+++ src/baekjoon/dp/Q9251.java
@@ -1,34 +1,27 @@
 package baekjoon.dp;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 //LCS
+//X
 public class Q9251 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String s1 = scan.nextLine();
-		String s2 = scan.nextLine();
-		List<Integer> a = new ArrayList<>();
-		int cnt = 0;
-		for(int k=0; k<s1.length(); k++) {
-			int index = 0;
-			cnt = 0;
-			for(int i=k; i<s1.length(); i++) {
-				for(int j=index; j<s2.length(); j++) {
-					if(s1.charAt(i)==s2.charAt(j)) {
-						index = j;
-						cnt++;
-						break;
-					}
+		char [] a = scan.nextLine().toCharArray();
+		char [] b = scan.nextLine().toCharArray();
+		
+		int [][] lcs = new int[a.length+1][b.length+1];
+		
+		for(int i=1; i<=a.length; i++) {
+			for(int j=1; j<=b.length; j++) {
+				if(a[i-1] == b[j-1]) {
+					lcs[i][j] = lcs[i-1][j-1]+1;
+				} else {
+					lcs[i][j] = Math.max(lcs[i][j-1], lcs[i-1][j]);
 				}
 			}
-			a.add(cnt);
 		}
-		Collections.sort(a);
-		System.out.println(a.get(a.size()-1));
+		System.out.println(lcs[a.length][b.length]);
 	}
 }
--- src/baekjoon/greedy/Q1700.java
+++ src/baekjoon/greedy/Q1700.java
@@ -7,49 +7,59 @@ import java.util.Scanner;
 //멀티탭 스케줄링
 //x
 public class Q1700 {
-	실패
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int m = scan.nextInt();
 		int [] a = new int[m];
-		int [] k = new int[101];
-		int cnt = 0;
+		int [] how = new int[101];//몇번 출현하는지 
 
 		for(int i=0; i<m; i++) {
 			a[i] = scan.nextInt();
-			k[a[i]]++;
+			how[a[i]]++;
 		}
 
+		int ans = 0;
 		List<Integer>list = new ArrayList<>();
+
 		for(int i=0; i<m; i++) {
-			k[a[i]]--;
-			if(list.contains(a[i])) {
-				continue; //이미 꽂혀있으면 continue
-			}
+			how[a[i]]--; //출현 횟수 감소
+
+			if(list.contains(a[i]))	continue; //이미 꽂혀있으면 continue
+
 			if(list.size()>=n) { //빼야될 때 
+				ans++;
 				boolean ok = false; //삭제 여부
 				for(int j=0; j<list.size(); j++) { //앞으로 사용하지 않는 수 찾기
-					if(k[list.get(j)] <= 0) {
+					if(how[list.get(j)] <= 0) { //출현 횟수가 0보다 작으면 앞으로 안나옴
 						list.remove(new Integer(list.get(j)));
-						cnt++;
 						ok = true; 
 						break;
 					}
 				}
-				if(!ok) { //앞으로 사용하지 않는 수가 없을 시
-					for(int j=m-1; j > i; j--) { //가장 나중에 사용하는 수 찾기
-						if(list.contains(a[j])){
-							list.remove(new Integer(a[j]));
-							cnt++;
-							break;
+				if(!ok) { //앞으로 사용하지 않는 수가 없을 시 가장 나중에 출현하는 수를 지운다
+					boolean [] b = new boolean[n]; //나중에 지워지는 수를 체크할 배열
+					int check = n;
+					loop:{ 
+						for(int j=i+1; j < m; j++) { 
+							for(int k=0; k<n; k++) {
+								if(list.get(k) == a[j] && !b[k]) {
+									b[k] = true;
+									check --;
+									if(check == 0) { //마지막으로 지워졌으면 지운다
+										list.remove(new Integer(a[j]));
+										break loop;
+									}
+									break;
+								}
+							}
 						}
 					}
 				}
 			}
 			list.add(a[i]);
 		}
-		System.out.println(cnt);
+		System.out.println(ans);
 	}
 }
