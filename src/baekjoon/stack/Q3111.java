5fe1e886d799facfaeef5f40afe94a6965820ea6
DunDung <ebseud6135@naver.com>
2019-07-03 17:45:16 +0900

    20190703

--- src/baekjoon/stack/Q3111.java
+++ src/baekjoon/stack/Q3111.java
@@ -6,89 +6,80 @@ import java.util.Stack;
 //검열
 //X
 public class Q3111 {
-
+	실패
 	public static void main(String[] args)  {
 		Scanner scan = new Scanner(System.in);
 		String key = scan.next();
 		String text = scan.next();
 		Stack<Character> left = new Stack<>();
 		Stack<Character> right = new Stack<>();
+		
 		int start = 0;
 		int end = text.length()-1;
 		boolean isRemove = false;
-
+		int leftKeyIdx = 0;
+		int rightKeyIdx = key.length()-1;
 		while(start <= end) {
-			
 			if(!isRemove) {
+				if(!left.isEmpty() && left.peek()==key.charAt(0)) {
+					leftKeyIdx++;
+				}
 				left.push(text.charAt(start++));
-				if(left.size() >= key.length() && left.peek() == key.charAt(key.length()-1)) {
-					int keyLen = key.length()-1;
-					boolean check = true;
-					for(int j=left.size()-1; j>=left.size()-key.length(); j--) {
-						if(left.get(j) != key.charAt(keyLen--)) {
-							check = false;
-							break;
-						}
-					}
-					if(check) {
-						isRemove = true;
-						for(int j=0; j<key.length(); j++) {
-							left.pop();
-						}
-
+				if(left.peek() == key.charAt(leftKeyIdx)) {
+					leftKeyIdx++;
+				}else {
+					leftKeyIdx = 0;
+				}
+				if(leftKeyIdx == key.length()) {
+					leftKeyIdx = 0;
+					isRemove = true;
+					for(int j=0; j<key.length(); j++) {
+						left.pop();
 					}
 				}
 			}
+			
 			if(isRemove && start <= end) {
-				String keyRev = new StringBuilder(key).reverse().toString();
 				right.push(text.charAt(end--));
-				if(right.size() >= key.length() && right.peek() == keyRev.charAt(key.length()-1)) {
-					int keyLen = key.length()-1;
-					boolean check = true;
-					for(int j=right.size()-1; j>=right.size()-key.length(); j--) {
-						if(right.get(j) != keyRev.charAt(keyLen--)) {
-							check = false;
-							break;
-						}
+				if(right.peek() != key.charAt(rightKeyIdx)) {
+					rightKeyIdx = key.length()-1;
+				} 
+				if(right.peek() == key.charAt(rightKeyIdx)) {
+					rightKeyIdx--;
+				}else {
+					rightKeyIdx = key.length()-1;
+				} 
+				if(rightKeyIdx == -1) {
+					rightKeyIdx = key.length()-1;
+					isRemove = false;
+					for(int j=0; j<key.length(); j++) {
+						right.pop();
 					}
-					if(check) {
-						for(int j=0; j<key.length(); j++) {
-							right.pop();
-						}
-						isRemove = false;
-					}
-
 				}
 			}
-//			System.out.println(start +" "+end);
-//			System.out.println(left.toString());
-//			System.out.println(right.toString());
 		}
-		int leftSize = left.size();
-		for(int i=0; i<leftSize; i++) {
+		while(!left.isEmpty()) {
 			right.push(left.pop());
 		}
-		int rightSize = right.size();
-		for(int i=0; i<rightSize; i++) {
-			if(right.size() >= i+key.length() && right.get(i) == key.charAt(key.length()-1)) {
-				boolean check = true;
-				int keyLen = key.length()-1;
-				for(int j=i; j<i+key.length(); j++) {
-					if(right.get(j) != key.charAt(keyLen--)){
-						check = false;
-						break;
-					}
-				}
-				if(check) {
-					for(int j=0; j<key.length(); j++) {
-						right.remove(i);
-					}
-					rightSize -= key.length();
+		
+		Stack<Character> ans = new Stack<>();
+		int ansKeyLen = 0;
+		while(!right.isEmpty()) {
+			ans.push(right.pop());
+			if(ans.peek() == key.charAt(ansKeyLen)) {
+				ansKeyLen++;
+			} else {
+				ansKeyLen = 0;
+			}
+			if(ansKeyLen == key.length()) {
+				ansKeyLen = 0;
+				for(int j=0; j<key.length(); j++) {
+					ans.pop();
 				}
 			}
 		}
-		while(!right.isEmpty()) {
-			System.out.print(right.pop());
+		for(int i=0; i<ans.size(); i++) {
+			System.out.print(ans.get(i));
 		}
 	}
 }
