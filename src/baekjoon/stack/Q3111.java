01e6f1793c32a233d1dd16604f18b3fa2c810f3b
DunDung <ebseud6135@naver.com>
2019-07-05 03:32:57 +0900

    20190705

--- src/baekjoon/stack/Q3111.java
+++ src/baekjoon/stack/Q3111.java
@@ -6,29 +6,29 @@ import java.util.Stack;
 //검열
 //X
 public class Q3111 {
-	실패
+
 	public static void main(String[] args)  {
 		Scanner scan = new Scanner(System.in);
 		String key = scan.next();
 		String text = scan.next();
 		Stack<Character> left = new Stack<>();
 		Stack<Character> right = new Stack<>();
-		
+
 		int start = 0;
 		int end = text.length()-1;
 		boolean isRemove = false;
 		int leftKeyIdx = 0;
 		int rightKeyIdx = key.length()-1;
 		while(start <= end) {
-			if(!isRemove) {
-				if(!left.isEmpty() && left.peek()==key.charAt(0)) {
-					leftKeyIdx++;
-				}
+			while(start <= end) {
 				left.push(text.charAt(start++));
 				if(left.peek() == key.charAt(leftKeyIdx)) {
 					leftKeyIdx++;
 				}else {
 					leftKeyIdx = 0;
+					if(left.peek() == key.charAt(0)) {
+						leftKeyIdx = 1;
+					} 
 				}
 				if(leftKeyIdx == key.length()) {
 					leftKeyIdx = 0;
@@ -36,18 +36,24 @@ public class Q3111 {
 					for(int j=0; j<key.length(); j++) {
 						left.pop();
 					}
+					if(!left.isEmpty() && left.peek()==key.charAt(0)) {
+						leftKeyIdx++;
+					}
+					break;
 				}
 			}
-			
-			if(isRemove && start <= end) {
+
+
+			while(start <= end) {
+
 				right.push(text.charAt(end--));
-				if(right.peek() != key.charAt(rightKeyIdx)) {
-					rightKeyIdx = key.length()-1;
-				} 
 				if(right.peek() == key.charAt(rightKeyIdx)) {
 					rightKeyIdx--;
 				}else {
 					rightKeyIdx = key.length()-1;
+					if(right.peek() == key.charAt(key.length()-1)) {
+						rightKeyIdx--;
+					}
 				} 
 				if(rightKeyIdx == -1) {
 					rightKeyIdx = key.length()-1;
@@ -55,32 +61,26 @@ public class Q3111 {
 					for(int j=0; j<key.length(); j++) {
 						right.pop();
 					}
+					if(!right.isEmpty() && right.peek() == key.charAt(key.length()-1)) {
+						rightKeyIdx--;
+					}
+					break;
 				}
 			}
 		}
-		while(!left.isEmpty()) {
-			right.push(left.pop());
-		}
-		
-		Stack<Character> ans = new Stack<>();
-		int ansKeyLen = 0;
 		while(!right.isEmpty()) {
-			ans.push(right.pop());
-			if(ans.peek() == key.charAt(ansKeyLen)) {
-				ansKeyLen++;
-			} else {
-				ansKeyLen = 0;
-			}
-			if(ansKeyLen == key.length()) {
-				ansKeyLen = 0;
-				for(int j=0; j<key.length(); j++) {
-					ans.pop();
-				}
-			}
+			left.push(right.pop());
 		}
-		for(int i=0; i<ans.size(); i++) {
-			System.out.print(ans.get(i));
+
+		String ans = "";
+		for(int i=0; i<left.size(); i++) {
+			ans+=left.get(i);
 		}
+		while(true) {
+			int idx = ans.indexOf(key);
+			if(idx < 0) break;
+			ans = new StringBuilder(ans).delete(idx, idx+key.length()).toString();
+		}
+		System.out.println(ans);
 	}
 }
-
