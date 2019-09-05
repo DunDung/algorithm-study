b95c8b8be4ff4661c099c2eaf28832d91a5899f3
DunDung <ebseud6135@naver.com>
2019-09-06 00:28:41 +0900

    20190906

--- src/baekjoon/implementation/Q3613.java
+++ src/baekjoon/implementation/Q3613.java
@@ -1,9 +1,59 @@
 package baekjoon.implementation;
 
+import java.util.Scanner;
+
+//Java vs C++
+//반례를 다생각하지 못함
+//첫글자가 소문자가 아닌경우
+//_가 연속으로 나올경우
+//마지막이 _인경우
+//알파벳, _가 아닌 다른 문자가 올경우
+//_다음 대문자가 올경우
+//모두 소문자는 가능
 public class Q3613 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder(sc.next());
+		if(!(sb.charAt(0)>='a' && sb.charAt(0)<='z')) {
+			System.out.println("Error!");
+		} else if(sb.indexOf("__")>=0){
+			System.out.println("Error!");
+		} else if(sb.charAt(sb.length()-1)=='_'){
+			System.out.println("Error!");
+		}else {
+			boolean java = false, c = false;
+			for(int i=0; i<sb.length(); i++) {
+				if(sb.charAt(i)=='_') {
+					c = true;
+					if(sb.charAt(i+1) >='A' && sb.charAt(i+1)<='Z') {
+						java= true;
+						break;
+					}
+					sb.delete(i, i+1);
+					sb.replace(i, i+1, sb.substring(i, i+1).toUpperCase());
+				}else if(sb.charAt(i)>='A' && sb.charAt(i)<='Z') {
+					java = true;
+					if(i+1 < sb.length() && sb.charAt(i+1) =='_') {
+						c= true;
+						break;
+					}
+					sb.replace(i, i+1, sb.substring(i, i+1).toLowerCase());
+					sb.insert(i, '_');
+				}else if(Character.isLetter(sb.charAt(i))) {
+					continue;
+				} else {
+					java =true;
+					c= true;
+					break;
+				}
+			}
+			if((java && c)) {
+				System.out.println("Error!");
+			} else {
+				System.out.println(sb.toString());
+			}
+		}
 	}
 
 }
--- src/kakao/T2019/Q6.java
+++ src/kakao/T2019/Q6.java
@@ -3,13 +3,12 @@ package kakao.T2019;
 import java.util.*;
 
 //매칭점수
-
 public class Q6 {
 	class Page {
 		int idx;
 		int basic, link;
 		double score;
-		
+
 		public Page(int idx, int basic, int link, double score) {
 			this.idx = idx;
 			this.basic = basic;
@@ -17,9 +16,69 @@ public class Q6 {
 			this.score = score;
 		}
 	}
+	class Comp implements Comparator<Page>{
+		public int compare(Page a, Page b) {
+			if(a.score == b.score) {
+				return a.idx - b.idx;
+			} else if(a.score < b.score) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+	}
 	public int solution(String word, String[] pages) {
-		
-		return 0;
+		int wsize = word.length();
+		Map<String, Integer>pageMap = new HashMap<>();
+		List<Page> pageList = new ArrayList<>();
+		word = word.toLowerCase();
+		for(int i=0; i<pages.length; i++) {
+			StringBuilder sb = new StringBuilder(pages[i].toLowerCase());
+			int mid = 0, posl = 0, posr = 0;
+			while(mid <= posl) {
+				posl = sb.indexOf("<meta", posl+1); // +1 을해야 똑같은걸 안찾는다.
+				posr = sb.indexOf(">", posl);
+				mid = sb.lastIndexOf("https://", posr); //뒤에서부터 찾는다.
+			}
+			posr = sb.indexOf("\"", mid);
+			String url = sb.substring(mid, posr);
+
+			posl = sb.indexOf("<body>", posr);
+			int basic = 0;
+			for(int start = posl; ;) {
+				start = sb.indexOf(word, start+1);
+				if(start < 0) break;
+				if(!Character.isLetter(sb.charAt(start-1)) && !Character.isLetter(sb.charAt(start+wsize))) {
+					basic++;
+					start += wsize;
+				}
+			}
+			int link = 0;
+			for(int start = posl; ;) {
+				start = sb.indexOf("<a href", start+1);
+				if(start < 0) break;
+				link++;
+			}
+			pageMap.put(url, i);
+			pageList.add(new Page(i, basic, link, (double)basic));
+		}
+		for(int i=0; i<pages.length; i++) {
+			StringBuilder sb = new StringBuilder(pages[i].toLowerCase());
+			for(int posl = 0, posr = 0; ;) {
+				posl = sb.indexOf("<a href", posr);
+				if(posl < 0) break;
+				posl = sb.indexOf("https://", posl);
+				posr = sb.indexOf("\"", posl);
+				String linkurl = sb.substring(posl, posr);
+
+				Integer value = pageMap.get(linkurl);
+				if(value != null) {
+					pageList.get(value).score += (double)pageList.get(i).basic / pageList.get(i).link;
+				}
+			}
+		}
+		pageList.sort(new Comp());
+		return pageList.get(0).idx;
 	}
 }
 
