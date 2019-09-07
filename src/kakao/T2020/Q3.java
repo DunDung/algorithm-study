cd566e5fddbd004fc22eee5dd08c1f6d52fd904c
DunDung <ebseud6135@naver.com>
2019-09-08 00:29:32 +0900

    20190908

---
+++ src/kakao/T2020/Q3.java
@@ -0,0 +1,15 @@
+package kakao.T2020;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+
+	}
+	public boolean solution(int[][] key, int[][] lock) {
+		boolean answer = true;
+		return answer;
+	}
+	public static boolean isCheck(int [][] key) {
+		for(int i=0;)
+	}
+}
