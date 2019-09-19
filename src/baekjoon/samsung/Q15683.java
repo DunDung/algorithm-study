c932a8bec5ef21f6808b61efb178f3400e821193
DunDung <ebseud6135@naver.com>
2019-09-19 20:06:50 +0900

    20190919

--- src/baekjoon/samsung/Q15683.java
+++ src/baekjoon/samsung/Q15683.java
@@ -24,65 +24,68 @@ public class Q15683 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
 		dfs(0, -1);
 		System.out.println(min);
 	}
 
 	static void dfs(int idx, int cnt) {
-		if(idx == n * m) {
+		if(idx == n * m) { //배열의 끝까지 도착
 			min = Math.min(min, count());
 			return;
 		}
+		
 		int r = idx / m;
 		int c = idx % m;
-		if(map[r][c] == 1) {
+		
+		if(map[r][c] == 1) { //0, 1, 2, 3
 			for(int i=0; i<4; i++) {
-				move(idx, i, 0, cnt);
+				monitor(idx, i, 0, cnt);
 				dfs(idx+1, cnt-1);
-				move(idx, i, cnt, 0);
+				monitor(idx, i, cnt, 0);
 			}
-		} else if(map[r][c] == 2) {
+		} else if(map[r][c] == 2) { //01, 23
 			for(int i=0; i<4; i+=2) {
-				move(idx, i, 0, cnt);
-				move(idx, i+1, 0, cnt);
+				monitor(idx, i, 0, cnt);
+				monitor(idx, i+1, 0, cnt);
 				dfs(idx+1, cnt-1);
-				move(idx, i, cnt, 0);
-				move(idx, i+1, cnt, 0);
+				monitor(idx, i, cnt, 0);
+				monitor(idx, i+1, cnt, 0);
 			}
-		} else if(map[r][c] == 3) {
+		} else if(map[r][c] == 3) { //02, 03, 12, 13
 			for(int i=0; i<2; i++) {
-				move(idx, i, 0, cnt);
+				monitor(idx, i, 0, cnt);
 				for(int j=2; j<4; j++) {
-					move(idx, j, 0, cnt);
+					monitor(idx, j, 0, cnt);
 					dfs(idx+1, cnt-1);
-					move(idx, j, cnt, 0);
+					monitor(idx, j, cnt, 0);
 				}
-				move(idx, i, cnt, 0);
+				monitor(idx, i, cnt, 0);
 			}
-		} else if(map[r][c] == 4) {
+		} else if(map[r][c] == 4) { //012, 013, 023, 123
 			for(int i=3; i>=0; i--) {
 				for(int j=0; j<4; j++) {
 					if(i==j) continue;
-					move(idx, j, 0, cnt);
+					monitor(idx, j, 0, cnt);
 				}
 				dfs(idx+1, cnt-1);
 				for(int j=0; j<4; j++) {
 					if(i==j) continue;
-					move(idx, j, cnt, 0);
+					monitor(idx, j, cnt, 0);
 				}
 			}
-		} else if(map[r][c] == 5) {
+		} else if(map[r][c] == 5) { //0123
 			for(int i=0; i<4; i++) {
-				move(idx, i, 0, cnt);
+				monitor(idx, i, 0, cnt);
 			}
 			dfs(idx+1, cnt-1);
 			for(int i=0; i<4; i++) {
-				move(idx, i, cnt, 0);
+				monitor(idx, i, cnt, 0);
 			}
-		} else dfs(idx+1, cnt-1);
+		} else dfs(idx+1, cnt-1); //1~5까지 수가 아닐 때 다음 수로 호출
 	}
 	
-	static void move(int idx, int dir, int orignal, int change) {
+	static void monitor(int idx, int dir, int orignal, int change) { //한가지 방향만 채워줌
 		int r = idx / m;
 		int c = idx % m;
 		while(true) {
