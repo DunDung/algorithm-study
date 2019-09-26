c3fd957da7bd2fe1321d8a8ae69a4bd1777e1b32
DunDung <ebseud6135@naver.com>
2019-09-26 20:35:13 +0900

    20190926

--- src/math/Euclidean.java
+++ src/math/Euclidean.java
@@ -18,11 +18,10 @@ public class Euclidean {
 		return a;
 	}
 	public static void main(String[] args) {
-		int a = 16;
-		int b = 24;
-		System.out.println(gcd1(a, b));
-		System.out.println(gcd2(a, b));
-		System.out.println(Math.sqrt(7));
+		int a = 1000000000-1;
+		int b = 1;
+		b = gcd1(a, b);
+		System.out.println(b);
 	}
 
 }
