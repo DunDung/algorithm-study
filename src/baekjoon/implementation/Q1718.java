fe1525fa5aa1be2e6fd4b44a867401f2587da95e
JunSeong <ebseud6135@naver.com>
2019-11-26 19:27:20 +0900

    Create Q1718.java

---
+++ src/baekjoon/implementation/Q1718.java
@@ -0,0 +1,28 @@
+package baekjoon.implementation;
+
+import java.util.*;
+import java.io.*;
+
+//¾ÏÈ£
+public class Q1718 {
+	static final int MAX_ARPABET = 26;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		String key = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<input.length(); i++) {
+			char tempChar = input.charAt(i);
+			if(Character.isLetter(tempChar)) {
+				int tempNum = tempChar - key.charAt(i%key.length());
+				if(tempNum <= 0) {
+					tempNum += MAX_ARPABET; 
+				}
+				tempChar = (char)(tempNum+'a' - 1);
+			}
+			sb.append(tempChar);
+		}
+		System.out.println(sb.toString());
+	}
+}
