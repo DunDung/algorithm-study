b45dc2983ac390c72333ed7b24c4d2fb9e63dc58
JunSeong <ebseud6135@naver.com>
2020-07-03 14:20:14 +0900

    124ëfeat: 124나라의 숫자 문제 ì풀이 추가

---
+++ src/programmers/lv2/NumberOf124World.java
@@ -0,0 +1,23 @@
+package programmers.lv2;
+
+public class NumberOf124World {
+
+    public static void main(String[] args) {
+        System.out.println(solution(16));
+    }
+
+    public static String solution(int n) {
+        StringBuilder builder = new StringBuilder();
+        while (n > 0) {
+            int 나머지 = n % 3;
+            if (나머지 == 0) {
+                n--;
+                builder.insert(0, "4");
+            } else {
+                builder.insert(0, 나머지);
+            }
+            n /= 3;
+        }
+        return builder.toString();
+    }
+}
