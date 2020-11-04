a88903f9cfd0c21a3cd8e55cb2bfcb5da7f408f3
JunSeong <ebseud6135@naver.com>
2020-11-04 11:11:15 +0900

    feat: 가장 큰 수 풀이 수정

--- src/programmers/lv2/가장큰수.java
+++ src/programmers/lv2/가장큰수.java
@@ -7,34 +7,22 @@ import java.util.stream.Collectors;
 // 2020.11.02
 public class 가장큰수 {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{40, 403}));
+        System.out.println(solution(new int[]{0, 0}));
     }
 
     public static String solution(int[] numbers) {
         Comparator<String> comparator = (o1, o2) -> {
-            int index1 = 0;
-            int index2 = 0;
-            char now1 = '0';
-            char now2 = '0';
-            while ((index1 < o1.length()) && (index2 < o2.length())) {
-                now1 = o1.charAt(index1);
-                now2 = o2.charAt(index2);
-                if (now1 == now2) {
-                    index1++;
-                    index2++;
-                    continue;
-                }
-                return (now1 - now2) * -1;
-            }
-            if (o1.charAt(o1.length() - 1) > o2.charAt(0)) {
-                return -1;
-            }
-            return o1.length() - o2.length();
+            String s1 = o1.concat(o2);
+            String s2 = o2.concat(o1);
+            return s1.compareTo(s2) * -1;
         };
         String answer = Arrays.stream(numbers)
                 .mapToObj(x -> String.valueOf(x))
                 .sorted(comparator)
                 .collect(Collectors.joining());
+        if (answer.charAt(0) == '0') {
+            return "0";
+        }
         return answer;
     }
 }
