198ee3249a97e507848c0675b35233bfb886ad49
JunSeong <ebseud6135@naver.com>
2020-11-23 21:22:58 +0900

    feat: 프로그래머스 level2 구명보트 문제 풀이 추가

---
+++ src/programmers/lv2/구명보트.java
@@ -0,0 +1,19 @@
+package programmers.lv2;
+
+import java.util.Arrays;
+
+public class 구명보트 {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+        int lightest = 0;
+        for (int heaviest = people.length - 1; heaviest >= lightest; heaviest--) {
+            answer++;
+            if (people[lightest] + people[heaviest] > limit) {
+                continue;
+            }
+            lightest++;
+        }
+        return answer;
+    }
+}
