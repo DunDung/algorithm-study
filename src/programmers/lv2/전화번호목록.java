c206e554716c51c8db070aec8a3c3238a0dc0a36
JunSeong <ebseud6135@naver.com>
2020-11-30 19:30:57 +0900

    feat: 프로그래머스 level2 전화번호 목록 문제 풀이 추가

---
+++ src/programmers/lv2/전화번호목록.java
@@ -0,0 +1,23 @@
+package programmers.lv2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+public class 전화번호목록 {
+    public boolean solution(String[] phoneBook) {
+        Set<String> hash = new HashSet<>();
+        Arrays.sort(phoneBook, Comparator.comparingInt(String::length));
+        for (String phoneNumber : phoneBook) {
+            for (int i = 0; i < phoneNumber.length(); i++) {
+                String sub = phoneNumber.substring(0, i + 1);
+                if (hash.contains(sub)) {
+                    return false;
+                }
+            }
+            hash.add(phoneNumber);
+        }
+        return true;
+    }
+}
