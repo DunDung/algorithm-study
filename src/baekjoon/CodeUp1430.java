f9b5b2eb5c5117f91945aacd06bcfc28a63244fb
dundung <ebseud6135@naver.com>
2022-02-11 13:18:36 +0900

    알고리즘 문제풀이 추가

--- src/baekjoon/CodeUp1430.java
+++ src/baekjoon/CodeUp1430.java
@@ -1,48 +1,22 @@
 package baekjoon;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class CodeUp1430 {
-    private static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            map.put(scanner.nextInt(), 1);
-        }
-
-        int M = scanner.nextInt();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int 질문입력 = scanner.nextInt();
-            if (map.get(질문입력) != null) {
-                stringBuilder.append(1);
-            } else {
-                stringBuilder.append(0);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = scan.nextLine();
+            String[] splits = str.split(" ");
+            for (String split : splits) {
+                StringBuilder temp = new StringBuilder(split);
+                answer.append(temp.reverse() + " ");
             }
-            stringBuilder.append(" ");
+            answer.append("\n");
         }
-        System.out.println(stringBuilder);
-    }
-
-    private void solution(Node node, int sum) {
-        if (node.left == null && node.right == null) {
-            int min = Math.min(sum, CodeUp1430.min);
-        }
-    }
-
-    class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data) {
-            this.data = data;
-        }
+        System.out.println(answer);
     }
 }
